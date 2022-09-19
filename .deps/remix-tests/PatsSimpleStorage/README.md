c8f064a4938feabc03d4377f2e0ce496abadb009
Remix Workspace <>
2022-09-19 18:20:20 +0100

    "to 1:45:22 comp & dep cont, store then retr favNum"

--- .deps/remix-tests/PatsSimpleStorage/README.md
+++ .deps/remix-tests/PatsSimpleStorage/README.md
@@ -1,2 +1,3 @@
 # PatsRemixBlockchain
 Dynamic storage for Remix simplestorage.sol exercise
+"to 1:38:40 see 5 contract items to favourite address but use only favouriteNumber" 
--- .deps/remix-tests/PatsSimpleStorage/simpleStorage.sol
+++ .deps/remix-tests/PatsSimpleStorage/simpleStorage.sol
@@ -6,3 +6,12 @@
 
 
 pragma solidity >=0.6.0 <0.9.0;
+
+contract simpleStorage{
+    uint256 favouriteNumber = 5;
+    // bool favouriteBool = true;
+    // string favouriteString = "String";
+    // int256 favouriteInt = -5;
+    // address favouriteAddress = 0x75773071458Df6F83cFb6E02586Ff992Cf736709; //Etherum address
+    // bytes32 favouriteBytes = "Cat";
+}
