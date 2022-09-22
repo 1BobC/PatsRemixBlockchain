bccb0ebf22a00f50f5d4d6342609bcb031b65ca7
Remix Workspace <>
2022-09-22 16:30:48 +0100

    coded retrieve and rem favNum, now structs for 1person and an array

--- .deps/remix-tests/PatsSimpleStorage/README.md
+++ .deps/remix-tests/PatsSimpleStorage/README.md
@@ -1,3 +1,8 @@
 # PatsRemixBlockchain
 Dynamic storage for Remix simplestorage.sol exercise
 "to 1:38:40 see 5 contract items to favourite address but use only favouriteNumber" 
+"to 1:45:22 comp & dep cont, store then retr favNum"
+"coded retrieve and rem favNum, now structs for 1person and an array plus discussion 
+of memory,short term versus storage,persistant, Oh, and strings are arrays of bytes therefore
+they are objects and we have to call their storage type This compiles and deploys to addP
+store people retrieve - okay!"
--- .deps/remix-tests/PatsSimpleStorage/simpleStorage.sol
+++ .deps/remix-tests/PatsSimpleStorage/simpleStorage.sol
@@ -8,10 +8,33 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 contract simpleStorage{
-    uint256 favouriteNumber = 5;
-    // bool favouriteBool = true;
+
+    //this will be initialized as 0
+    //uint256 public favouriteNumber;
+    uint256 favouriteNumber; //removing public removes blue favouriteNumber blue button
+    bool favouriteBool;      //included when creating a list of people
     // string favouriteString = "String";
     // int256 favouriteInt = -5;
     // address favouriteAddress = 0x75773071458Df6F83cFb6E02586Ff992Cf736709; //Etherum address
     // bytes32 favouriteBytes = "Cat";
+    struct People {
+        uint256 favouriteNumber;
+        string name;
+    }
+    //People public person = People({favouriteNumber: 11224, name: "Robert"}); 
+    //commented out when creating a list of people
+
+    People[] public people; //this [] is a dynamic array, [22] is fixed
+
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber; 
+    }
+        //keywords view and pure require no transaction therefore no cost
+    function retrieve() public view returns(uint256) {
+        return favouriteNumber;
+    }
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        people.push(People({favouriteNumber: _favouriteNumber, name: _name}));
+        //shorter version can drop favouriteNumbe and name text
+    }
 }
